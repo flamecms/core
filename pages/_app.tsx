2cd9fce14745286237f15f608dac60a56c1838b4
Dani <git@danny.works>
2023-02-08 14:42:51 -0800

    remove loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,21 +27,6 @@ const Loading: Function = function () {
 
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-        const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},5000);
-
-        router.events.on('routeChangeStart', handleStart)
-        router.events.on('routeChangeComplete', handleComplete)
-        router.events.on('routeChangeError',  handleComplete)
-
-        return () => {
-            router.events.off('routeChangeStart', handleStart)
-            router.events.off('routeChangeComplete', handleComplete)
-            router.events.off('routeChangeError', handleComplete)
-        }
-    })
-
     return loading && (<div className='spinner-wrapper dark:bg-accent'> <div className="spinner"></div></div>)
 }
 
--- pages/forum/thread/[thread_id]/index.tsx
+++ pages/forum/thread/[thread_id]/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useRouter } from 'next/router'
 import { supabase } from "../../../../lib/supabase"
 import useUser from "../../../../hooks/useUser"
